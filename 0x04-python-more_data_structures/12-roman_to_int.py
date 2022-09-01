b9d78cf9fdc01a5ac1e29506d4dea77cdf063a33
Incogany <tigerclaw373@gmail.com>
2022-09-01 06:41:41 -0700

    .p

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = maximum = 0
+    for num in range(len(roman_string) -1, -1, -1):
+        if roman[roman_string[num]] >= maximum:
+            number += roman[roman_string[num]]
+        else:
+            number -= roman[roman_string[num]]
+
+        maximum = roman[roman_string[num]]
+
+    return number
